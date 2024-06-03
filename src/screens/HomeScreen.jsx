3707e6115010dcff08c8dc09c5e8e2ea27db1fe3
Nezhinskiy22 <Nezhinskiy22@gmail.com>
2024-06-03 22:27:38 +0300

    first commit

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,154 @@
+import { Alert, Image, ScrollView, Text, TextInput, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { BellIcon, MagnifyingGlassIcon } from "react-native-heroicons/outline";
+import Categories from "./../components/Categories";
+import axios from "axios";
+import Recepies from "../components/Recepies";
+import { mealData } from "../constants";
+import Loading from "./../components/Loading";
+
+const HomeScreen = () => {
+  const [activeCategory, setActiveCategory] = useState("Beef");
+  const [categories, setCategories] = useState([]);
+  const [meals, setMeals] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getCategories();
+    getRecepies();
+  }, []);
+
+  const handleChangeCategory = (category) => {
+    getRecepies(category);
+    setActiveCategory(category);
+    setMeals([]);
+  };
+
+  const getCategories = async () => {
+    try {
+      const response = await axios.get(
+        "https://www.themealdb.com/api/json/v1/1/categories.php"
+      );
+      if (response && response.data) {
+        setCategories(response.data.categories);
+      }
+    } catch (error) {
+      console.log("Error", error.message);
+    }
+  };
+
+  const getRecepies = async (category = "Beef") => {
+    try {
+      const response = await axios.get(
+        `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`
+      );
+      if (response && response.data) {
+        setMeals(response.data.meals);
+      }
+    } catch (error) {
+      console.log("Error", error.message);
+    }
+  };
+
+  const getSearch = async (searchTerm) => {
+    setLoading(true);
+    if (searchTerm) {
+      try {
+        const response = await axios.get(
+          `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`
+        );
+        if (response && response.data) {
+          setMeals(response.data.meals);
+        }
+      } catch (error) {
+        console.log("Error", error.message);
+      }
+    } else if (!searchTerm) {
+      Alert.alert("Please enter serchterm");
+    }
+    setSearchTerm("");
+    setLoading(false);
+  };
+
+  return (
+    <View className="flex-1 bg-white">
+      <StatusBar style="dark" />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 50 }}
+        className="space-y-6 pt-14"
+      >
+        {/* avatar and bell icon */}
+        <View className="mx-4 flex-row justify-between items-center mb-2">
+          <Image
+            source={require("../../assets/images/avatar.png")}
+            style={{ height: hp(5), width: hp(5.5) }}
+          />
+          <BellIcon size={hp(4)} color="gray" />
+        </View>
+        {/* greetings and punchline */}
+        <View className="mx-4 space-y-2 mb-2">
+          <Text style={{ fontSize: hp(1.7) }} className="text-neutral-600">
+            Hello, Noman!
+          </Text>
+          <View>
+            <Text
+              style={{ fontSize: hp(3.8) }}
+              className="font-semibold text-neutral-600"
+            >
+              Make your own food,
+            </Text>
+          </View>
+          <Text
+            style={{ fontSize: hp(3.8) }}
+            className="font-semibold text-neutral-600"
+          >
+            stay at <Text className="text-amber-400">home</Text>
+          </Text>
+        </View>
+        {/* search bar */}
+        <View className="mx-4 flex-row items-center rounded-full bg-black/5 p-[6px]">
+          <TextInput
+            defaultValue={searchTerm}
+            onChangeText={(value) => setSearchTerm(value)}
+            placeholder="Search any recepy"
+            placeholderTextColor={"gray"}
+            style={{ fontSize: hp(1.7) }}
+            className="flex-1 text-base mb-1 pl-3 tracking-wider"
+          />
+          <View className="bg-white rounded-full p-3">
+            <MagnifyingGlassIcon
+              size={hp(2.5)}
+              strokeWidth={3}
+              color="gray"
+              onPress={() => getSearch(searchTerm)}
+            />
+          </View>
+        </View>
+        {/* categories */}
+        <View>
+          {categories.length > 0 && (
+            <Categories
+              activeCategory={activeCategory}
+              handleChangeCategory={handleChangeCategory}
+              categories={categories}
+            />
+          )}
+          {loading ? <Loading size="large" className="mt-2" /> : null}
+        </View>
+        {/* recepies */}
+        <View>
+          <Recepies categories={categories} meals={meals} />
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default HomeScreen;
